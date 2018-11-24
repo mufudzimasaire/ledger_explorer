47f9ae89ec35c6ca17ec6aa664a1da7833382970
Mufudzi Masaire <mufudzimasaire@gmail.com>
2018-11-24 20:34:33 +0000

    Add fetchAccountInformation action

---
+++ actions/index.js
@@ -0,0 +1,26 @@
+// Libs
+import LedgerClient from '../lib/ledger-client'
+
+// Action Types
+import { FETCH_ACCOUNT_INFORMATION } from '../actions/action-types'
+
+// Constants
+const ledgerAPIClient = new LedgerClient()
+
+export const fetchAccountInformation = (address) => {
+  return (dispatch) => {
+    return (
+      ledgerAPIClient.lookup(address)
+        .then((accountInformation) => {
+          console.log({accountInformation})
+          dispatch({
+            type: FETCH_ACCOUNT_INFORMATION,
+            payload: accountInformation
+          })
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    )
+  }
+}
