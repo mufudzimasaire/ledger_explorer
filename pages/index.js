e535fdb22f3e713b84db18d4b66288b06e889ea4
Mufudzi Masaire <mufudzimasaire@gmail.com>
2018-11-24 15:43:53 +0000

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+// Libraries
+import { Component } from 'react'
+
+class Index extends Component {
+  render () {
+    return (
+      <div>
+        Welcome to the XRP ledger explorer!
+      </div>
+    )
+  }
+}
+
+export default Index
