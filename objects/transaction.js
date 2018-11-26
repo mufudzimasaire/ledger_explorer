e657f2ef66e61f6d9a00d838548250ccaf2004b0
Mufudzi Masaire <mufudzimasaire@gmail.com>
2018-11-26 14:04:15 +0000

    Render minimum transaction details

--- objects/transaction.js
+++ objects/transaction.js
@@ -4,8 +4,13 @@ import { PureComponent } from 'react'
 class Transaction extends PureComponent {
   render () {
     const { transaction } = this.props
+    const specification = transaction.specification
     return (
-      <p>{ JSON.stringify(transaction) }</p>
+      <div className="o-transaction">
+        <p>Amount: { specification.source.maxAmount.value }  { specification.source.maxAmount.currency }</p>
+        <p>Destination: { specification.destination.address }</p>
+        <p>Fee: { transaction.outcome.fee } xrp</p>
+      </div>
     )
   }
 }
